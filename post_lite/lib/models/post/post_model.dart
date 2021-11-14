cea7328e97503de0fed9fb96f01fa62095af66ae
PavelParamon <43481576+PavelParamon@users.noreply.github.com>
2021-11-14 23:26:23 +0300

    modify PostModel

--- post_lite/lib/models/post/post_model.dart
+++ post_lite/lib/models/post/post_model.dart
@@ -3,6 +3,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'post_model.freezed.dart';
 
 @freezed
-abstract class PostModel with _$PostModel{
-  const factory PostModel({required int id, required String title, required String body, required int userId}) = _PostModel;
+abstract class PostModel with _$PostModel {
+  const factory PostModel(
+      {required int id,
+      required String title,
+      required String body,
+      required int userId}) = _PostModel;
 }
--- post_lite/lib/services/post/post_repository.dart
+++ post_lite/lib/services/post/post_repository.dart
@@ -1,7 +1,21 @@
 import 'package:post_lite/models/post/post_model.dart';
 import 'package:post_lite/services/post/post_provider.dart';
 
-class PostRepository{
-  PostProvider _postProvider = PostProvider();
-  Future<List<PostModel>> getPosts() => _postProvider.getPosts();
+class PostRepository {
+  //PostProvider _postProvider = PostProvider();
+
+  //Future<List<PostModel>> getPosts() => _postProvider.getPosts();
+
+  Future<List<PostModel>> getPosts() async {
+    List<PostModel> listPosts = [];
+    for (int i = 0; i < 10; i++) {
+      listPosts.add(PostModel(
+          id: i + 1,
+          title: "Hello World!",
+          body:
+              "Zebra loach bleak pilchard dwarf loach cow shark; ballan wrasse freshwater hatchetfish. Rio Grande perch deep sea smelt; bamboo shark; whitebait Sacramento blackfish denticle herring Pacific hake, grunt damselfish; tenpounder livebearer",
+          userId: i));
+    }
+    return Future.delayed(Duration(seconds: 1), () => listPosts);
+  }
 }
