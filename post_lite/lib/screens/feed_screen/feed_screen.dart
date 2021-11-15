170a4835cd958f122b024ff8f1397460128e7408
PavelParamon <43481576+PavelParamon@users.noreply.github.com>
2021-11-15 16:02:49 +0300

    modify feed_screen

--- post_lite/lib/screens/feed_screen/feed_screen.dart
+++ post_lite/lib/screens/feed_screen/feed_screen.dart
@@ -2,15 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+import 'package:post_lite/config/config.dart';
 import 'package:post_lite/models/post/post_model.dart';
 import 'package:post_lite/models/user/user_model.dart';
+import 'package:post_lite/screens/authentication_screens/main_login/main_login_screen.dart';
 import 'package:post_lite/screens/feed_screen/bloc/feed_screen_bloc.dart';
+import 'package:post_lite/widgets/big_button_redirect.dart';
+
 import '../../widgets/post/post_item.dart';
 
 class FeedScreen extends StatefulWidget {
   late bool isAuth = false;
 
   FeedScreen({Key? key, required this.isAuth}) : super(key: key);
+
   @override
   _FeedScreenState createState() => _FeedScreenState();
 }
@@ -38,7 +43,8 @@ class _FeedScreenState extends State<FeedScreen> {
                   child: CircularProgressIndicator(),
                 );
               },
-              showPosts: (List<PostModel> postsToShow, String postsToString, List<UserModel> userPost) {
+              showPosts: (List<PostModel> postsToShow, String postsToString,
+                  List<UserModel> userPost) {
                 viewToReturn = _showPostsBuilder(postsToShow, userPost);
               },
               errorLoading: () {
@@ -52,35 +58,58 @@ class _FeedScreenState extends State<FeedScreen> {
     );
   }
 
-  Widget _showPostsBuilder(List<PostModel> postToShow, List<UserModel> userPost){
-    return LazyLoadScrollView(
-      scrollOffset: (MediaQuery.of(context).size.height * 0.7).toInt(),
-      child: ListView.builder(
-        padding: EdgeInsets.all(16),
-        shrinkWrap: true,
-        physics: BouncingScrollPhysics(),
-        itemCount: postToShow.length + 1,
-        itemBuilder: (BuildContext context, int index) {
-          if (index.isOdd) return const Divider();
-          if (index != postToShow.length) {
-            PostModel post = postToShow[index];
-            UserModel user = userPost[post.userId];
-            return PostItem(
-              post: post,
-              user: user,
-            );
-          } else{
+  Widget _showPostsBuilder(
+      List<PostModel> postToShow, List<UserModel> userPost) {
+    return Stack(
+      children: [
+        LazyLoadScrollView(
+          scrollOffset: (MediaQuery.of(context).size.height * 0.7).toInt(),
+          child: ListView.builder(
+            padding: EdgeInsets.all(16),
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemCount: postToShow.length + 1,
+            itemBuilder: (BuildContext context, int index) {
+              if (index.isOdd) return const Divider();
+              if (index != postToShow.length) {
+                PostModel post = postToShow[index];
+                UserModel user = userPost[post.userId];
+                return PostItem(
+                  post: post,
+                  user: user,
+                );
+              } else {
+                _feedScreenBloc.add(FeedScreenEvent.loadMore());
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          onEndOfPage: () {
             _feedScreenBloc.add(FeedScreenEvent.loadMore());
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-        },
-      ),
-      onEndOfPage: () {
-        _feedScreenBloc.add(FeedScreenEvent.loadMore());
-      },
+          },
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: BigButtonRedirect(
+              style: Config.styleBtn,
+              text: "Log In",
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    opaque: false,
+                    pageBuilder: (_, __, ___) => MainLogin(),
+                  ),
+                ),
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -96,5 +125,4 @@ class _FeedScreenState extends State<FeedScreen> {
       ),
     );
   }
-
 }
